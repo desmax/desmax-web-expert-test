29f2b9483382a441deedb6a8d2d68e9d0141a406
Maksym Pustynnikov <pustinnikov@gmail.com>
2024-12-21 08:22:46 +0100

    FixPostgreSQLDefaultSchemaListener

--- config/services.php
+++ config/services.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use App\Infra\FixPostgreSQLDefaultSchemaListener;
+use Doctrine\ORM\Tools\ToolEvents;
 
 return static function (ContainerConfigurator $configurator): void {
     $services = $configurator->services();
@@ -16,4 +18,8 @@ return static function (ContainerConfigurator $configurator): void {
             '../src/Domain/',
             '../src/Kernel.php'
         ]);
+
+    $services
+        ->set(FixPostgreSQLDefaultSchemaListener::class)
+        ->tag('doctrine.event_listener', ['event' => ToolEvents::postGenerateSchema]);
 };
