1660e318074577a0b226bcfbaa7c5733320a3719
Maksym Pustynnikov <pustinnikov@gmail.com>
2024-12-22 13:27:01 +0100

    asset mapper

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+import './styles/app.css';
+import './favicon/favicon-32x32.png';
+import './favicon/favicon-16x16.png';
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
